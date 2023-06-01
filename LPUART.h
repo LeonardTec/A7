19f6195be5d2349d137604ba2c2d150685112aae
pennyalioshin <133727229+pennyalioshin@users.noreply.github.com>
2023-06-01 09:05:15 -0700

    Update LPUART.h

--- LPUART.h
+++ LPUART.h
@@ -10,12 +10,12 @@
 #include "stm32l4xx_hal.h"
 #include <stdio.h>
 
-
+void LPUART_cursorMove(int spaces, int lines);
 void LPUART_init( void );
 void LPUART_Print( const char* );
 void LPUART1_IRQHandler( void );
 void LPUART_ESC_Print( const char*);
 void LPUART_write( void );
-void LPUART_moveCursor(int spaces, int lines);
+//void LPUART_moveCursor(int spaces, int lines);
 
 #endif /* INC_UART_H_ */
